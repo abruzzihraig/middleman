98174b33b8be96730cc00e1ecf7daaba4d5e4dd8
何洋 <yang.he@jimubox.com>
2015-05-15 18:04:22 +0800

    init: build scaffold with Yeoman & serve static files

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var koa = require('koa');
+var serve = require('koa-static');
+var path = require('path');
+var app_path = path.join(__dirname, 'fe', 'dist');
 var app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
-
+app.use(serve(app_path));
 app.listen(3000);
