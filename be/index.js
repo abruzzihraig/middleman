92d71f492f3118e5c30060a149cac02c1db55fc2
何洋 <yang.he@jimubox.com>
2015-05-22 19:05:00 +0800

    init: initialized rethinkdb

--- be/index.js
+++ be/index.js
@@ -4,6 +4,11 @@ var serve = require('koa-static');
 var path = require('path');
 var err_pages = require('./middlewares/404');
 var app_path = path.join(__dirname, '../fe/dist');
+var r = require('rethinkdbdash')({servers: [{host: 'localhost', port: 28015}]});
+
+r.db('man').table('users').insert([{name: "abc", age: 20}]).then(function() {
+    console.log(r.table('users'));
+})
 
 router.get('/test', function*(next) {
     this.body = 'test API';
