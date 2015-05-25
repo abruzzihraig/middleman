2abdf5f590fd48295c58d023b6d56227def26c8a
何洋 <yang.he@jimubox.com>
2015-05-25 12:36:37 +0800

    add: add body-parser

--- be/index.js
+++ be/index.js
@@ -2,16 +2,25 @@ var app = require('koa')();
 var router = require('koa-router')();
 var serve = require('koa-static');
 var path = require('path');
+var parser = require('koa-bodyparser')();
 var err_pages = require('./middlewares/404');
 var app_path = path.join(__dirname, '../fe/dist');
-var r = require('rethinkdbdash')({servers: [{host: 'localhost', port: 28015}]});
+var r = require('rethinkdbdash')();
 
-r.db('man').table('users').insert([{name: "abc", age: 20}]).then(function() {
-    console.log(r.table('users'));
-})
+app.use(parser);
 
 router.get('/test', function*(next) {
     this.body = 'test API';
+})
+
+router.post('/user/rigister', function*(next) {
+    try {
+        var all_users = yield r.db('man').table('users').run();
+        this.body = yield r.db('man').table('users').insert(this.request.body);
+    } catch (err) {
+        this.status = 500;
+        this.body = {err: err};
+    }
 })
 
 app.use(err_pages());
